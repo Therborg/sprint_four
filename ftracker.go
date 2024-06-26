9d456d82e44b74e90436661d5fb397f0d0399114
Часовников Кирилл <chasownickow.kir@yandex.ru>
2024-06-26 15:46:24 +0300

    second commit

--- ftracker.go
+++ ftracker.go
@@ -102,8 +102,8 @@ const (
 // height float64 — рост пользователя.
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
 	// ваш код здесь
-	return ((walkingCaloriesWeightMultiplier*weight + (math.Pow(meanSpeed(action, duration), 2)/height)*
-		0.029*weight) * duration * minInH)
+	return ((walkingCaloriesWeightMultiplier*weight + (math.Pow(meanSpeed(action, duration), 2)/height/cmInM)*
+		walkingSpeedHeightMultiplier*weight) * duration * minInH)
 }
 
 // Константы для расчета калорий, расходуемых при плавании.
